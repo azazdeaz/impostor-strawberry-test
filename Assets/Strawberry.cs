8e58b43711327310de3d10a280b6676a25fd8c26
Polgár András <azazdeaz@gmail.com>
2023-10-02 16:06:46 +0900

    Implement basic mesh building

--- Assets/Strawberry.cs
+++ Assets/Strawberry.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Obi;
+using Unity.VisualScripting;
 
 public class Bone
 {
@@ -29,6 +31,9 @@ public class PlantDescription
 
 public class Strawberry : MonoBehaviour
 {
+    
+    public Texture leafTexture;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +41,7 @@ public class Strawberry : MonoBehaviour
         var plantDescription = new PlantDescription();
         var up = Vector3.up;
         var side = Vector3.right;
+        var scale = 1.1f;
 
         plantDescription.vertices = new Vector3[]
         {
@@ -56,6 +62,10 @@ public class Strawberry : MonoBehaviour
             up * 1.3f + side * -0.1f,
             up * 1.4f + side * 0.0f,
         };
+        for (var i = 0; i < plantDescription.vertices.Length; i++)
+        {
+            plantDescription.vertices[i] *= scale;
+        }
         plantDescription.bones = new Bone[]
         {
             new Bone(0, 1),
@@ -75,10 +85,11 @@ public class Strawberry : MonoBehaviour
             new Bone(9, 12),
         };
         
-        var blade = new Blade();
-        blade.triangles = new uint[] { 6, 7, 8, 6, 8, 9, 6, 9, 12, 6, 12, 13, 6, 13, 14 };
-        plantDescription.blades = new Blade[] { blade };
-
+        var first_blade = new Blade();
+        first_blade.triangles = new uint[] { 6, 7, 8, 6, 8, 9, 6, 9, 12, 6, 12, 13, 6, 13, 14 };
+        plantDescription.blades = new Blade[] { first_blade };
+        
+        // Build bones
         var boneGameObject = new Dictionary<uint, GameObject>();
         var rootAxe = new GameObject("RootAxe");
         rootAxe.transform.parent = gameObject.transform;
@@ -88,7 +99,7 @@ public class Strawberry : MonoBehaviour
         foreach (Bone bone in plantDescription.bones) {
             var parent = boneGameObject[bone.start];
             // create a gameobject for each transform:
-            var axe = new GameObject("Axe_" + boneGameObject.Count);
+            var axe = new GameObject("Axe_" + bone.start + "_" + bone.end);
             axe.transform.parent = parent.transform;
             axe.transform.position = plantDescription.vertices[bone.end];// - plantDescription.vertices[bone.start];
             boneGameObject[bone.end] = axe;
@@ -99,6 +110,79 @@ public class Strawberry : MonoBehaviour
         boneParticleRenderer.radiusScale = 2.0f;
         boneParticleRenderer.particleColor = Color.green;
         ObiBoneBlueprint boneBlueprint = obiBone.boneBlueprint;
+        
+        
+        // Build meshes
+        foreach (Blade blade in plantDescription.blades)
+        {
+            var mesh = new Mesh();
+            mesh.vertices = plantDescription.vertices;
+            mesh.triangles = blade.triangles.Select(v => (int)v).ToArray();
+            mesh.RecalculateNormals();
+            
+            var bladeGameObject = new GameObject("Blade");
+            var bladeGameObjectMeshFilter = bladeGameObject.AddComponent<MeshFilter>();
+            bladeGameObjectMeshFilter.mesh = mesh;
+            bladeGameObjectMeshFilter.transform.parent = gameObject.transform;
+
+            // create the blueprint: (ObiClothBlueprint, ObiTearableClothBlueprint, ObiSkinnedClothBlueprint)
+            var clothBlueprint = ScriptableObject.CreateInstance<ObiClothBlueprint>();
+
+            // set the input mesh:
+            clothBlueprint.inputMesh = mesh;
+
+            // generate the clothBlueprint:
+            // StartCoroutine(clothBlueprint.Generate());
+            clothBlueprint.GenerateImmediate();
+
+            // create the cloth actor/renderer:
+            // GameObject clothObject = new GameObject("cloth", typeof(ObiCloth),typeof(ObiClothRenderer));
+            ObiCloth obiCloth = bladeGameObject.AddComponent<ObiCloth>();
+            bladeGameObject.AddComponent<ObiClothRenderer>();
+            bladeGameObject.AddComponent<MeshRenderer>();
+            
+            Renderer clothRenderer = gameObject.GetComponent<Renderer>();
+            // set material for the leaf
+            clothRenderer.material = new Material(Shader.Find("Unlit/Texture"));
+            clothRenderer.material.mainTexture = leafTexture;
+
+            // instantiate and set the clothBlueprint:
+            obiCloth.clothBlueprint = ScriptableObject.Instantiate(clothBlueprint);
+
+            // parent the cloth under a solver to start simulation:
+            obiCloth.transform.parent = gameObject.transform;
+
+            print("> cloth particles" + string.Join(", ", obiCloth.solverIndices));
+            
+            
+            // Stitch the blade to the bone
+            ObiStitcher stitcher = bladeGameObject.AddComponent<ObiStitcher>();
+            stitcher.Actor1 = obiCloth;
+            stitcher.Actor2 = obiBone;
+            // stitch each particle in the first row of the cloth to the closest particle in the rope:
+            for (int i = 0; i < boneBlueprint.activeParticleCount; ++i)
+            {
+                var position = boneBlueprint.positions[i];
+                // find the closest particle in the cloth:
+                int closestParticle = 0;
+                float closestDistance = float.MaxValue;
+                for (int j = 0; j < clothBlueprint.activeParticleCount; ++j)
+                {
+                    var distance = Vector3.Distance(position, clothBlueprint.positions[j]);
+                    if (distance < closestDistance)
+                    {
+                        closestParticle = j;
+                        closestDistance = distance;
+                    }
+                }
+                if (closestDistance > 0.1f)
+                {
+                    continue;
+                }
+                print("Stitch " + i + " to " + closestParticle + " at " + closestDistance + " distance");
+                stitcher.AddStitch(closestParticle, i);
+            }
+        }
     }
 
     // Update is called once per frame
