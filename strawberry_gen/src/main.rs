945e5a55e188fd144f7891dc2a5caa4672288362
Polgár András <azazdeaz@gmail.com>
2023-11-11 17:20:51 +0100

    Fix traversing nodes with Aery

--- strawberry_gen/src/main.rs
+++ strawberry_gen/src/main.rs
@@ -1,5 +1,7 @@
-use bevy::prelude::*;
+use std::hash::Hash;
+
 use aery::prelude::*;
+use bevy::{prelude::*, utils::HashMap};
 
 fn main() {
     let mut plant = StrawberryPlant::new();
@@ -11,12 +13,12 @@ struct StrawberryPlant {
 }
 
 #[derive(Component, Debug)]
-struct StemNode {
+struct Stem {
     size: f32,
     length: f32,
     rotation: Quat,
 }
-impl StemNode {
+impl Stem {
     fn new(size: f32, length: f32, rotation: Quat) -> Self {
         Self {
             size,
@@ -26,6 +28,14 @@ impl StemNode {
     }
     fn simple() -> Self {
         Self::new(1.0, 1.0, Quat::default())
+    }
+    fn with_size(mut self, size: f32) -> Self {
+        self.size = size;
+        self
+    }
+    fn with_length(mut self, length: f32) -> Self {
+        self.length = length;
+        self
     }
 }
 
@@ -42,20 +52,48 @@ impl StrawberryPlant {
         }
     }
     fn init(&mut self) {
-        self.world.spawn((StemNode::simple(), RootAxe)).scope::<AxisUp>(|scope| {
-            scope.add(StemNode::simple()).scope::<AxisUp>(|scope| {
-                scope.add(StemNode::simple());
+        self.world
+            .spawn((
+                Stem::simple().with_length(1.1),
+                Transform::default(),
+                RootAxe,
+            ))
+            .scope::<AxisUp>(|scope| {
+                scope
+                    .add((Stem::simple().with_length(1.2), Transform::default()))
+                    .scope::<AxisUp>(|scope| {
+                        scope.add((Stem::simple().with_length(1.3), Transform::default()));
+                    });
             });
-        });
 
         let update_transforms = self.world.register_system(update_stem_transforms);
-        self.world.run_system(update_transforms);
+        self.world.run_system(update_transforms).ok();
+        let print_stems = self.world.register_system(print_stems);
+        self.world.run_system(print_stems).ok();
     }
 }
 
-fn update_stem_transforms(roots: Query<Entity, With<RootAxe>>, mut stems: Query<(&StemNode, Relations<AxisUp>)>) {
-    stems.traverse_mut::<AxisUp>(roots.iter()).for_each(|ref mut stem, _| {
-        println!("Stem: {:?}", stem);
-        // TCF::Continue;
-    });
+fn update_stem_transforms(
+    // Orient the tree so the `Root`s are in the soil.
+    // Aery tracks `Root<R>`, `Branch<R>`, `Leaf<R>` (s) for you
+    roots: Query<Entity, Root<AxisUp>>, 
+    mut plants: Query<((&mut Transform, &Stem), Relations<AxisUp>)>,
+) {
+    plants
+        .traverse_mut::<AxisUp>(roots.iter())
+        .track_self()
+        .for_each(|(p_transf, _), _, (c_transf, stem), _| {
+            **c_transf = **p_transf * Transform::from_translation(Vec3::Y * stem.length);
+        });
+}
+
+fn print_stems(
+    roots: Query<Entity, With<RootAxe>>,
+    stems: Query<((&Stem, &Transform), Relations<AxisUp>)>,
+) {
+    stems
+        .traverse::<AxisUp>(roots.iter())
+        .for_each(|(stem, transform), _| {
+            println!("Stem: {:?} {:?}", stem, transform.translation);
+        });
 }
